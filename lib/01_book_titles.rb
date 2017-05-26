10bf20d0c169aeae964d035919479665d7b93026
LeoXugu <robinleoknauth@gmail.com>
2017-05-25 21:11:35 -0700

    classes, classy!

--- lib/01_book_titles.rb
+++ lib/01_book_titles.rb
@@ -1,3 +1,18 @@
 class Book
-  # TODO: your code goes here!
+  attr_accessor :title
+
+  def title
+    non_cap_words = %w(and in the of a an)
+    arr = []
+
+    @title.split.each_with_index do |word, i|
+      if non_cap_words.include?(word) && i != 0
+        arr << word
+      else
+        arr << word.capitalize
+      end
+    end
+    arr.join(" ")
+  end
+
 end
