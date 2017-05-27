c53c91b40cfd7f97e308510425c953e6d2d3100e
LeoXugu <robinleoknauth@gmail.com>
2017-05-27 14:44:17 -0700

    another exercise on classes

--- lib/03_temperature_object.rb
+++ lib/03_temperature_object.rb
@@ -1,4 +1,14 @@
 class Temperature
+  
+  def initialize(temperature)
+    if temperature.has_key?(:f)
+      self.fahrenheit = temperature[:f]
+    else
+      self.celsius = temperature[:c]
+    end
+  end
+
+
   def self.ftoc(temp)
     (temp - 32) * (5.0 / 9)
   end
@@ -7,5 +17,28 @@ class Temperature
     (temp / (5.0 / 9)) + 32
   end
 
+  def self.from_celsius(temperature)
+    self.new(:c => temperature)
+  end
+
+  def self.from_fahrenheit(temperature)
+    self.new(:f => temperature)
+  end
+
+  def in_fahrenheit
+    self.class.ctof(@degrees)
+  end
+
+  def in_celsius
+    @degrees
+  end
+
+  def celsius=(temperature)
+    @degrees = temperature
+  end
+
+  def fahrenheit=(temperature)
+    @degrees = self.class.ftoc(temperature)
+  end
 
 end
